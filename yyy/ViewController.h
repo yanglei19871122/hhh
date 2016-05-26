aeaab10037c28d3430602875626f45247f8c7261
yanglei19871122 <271943358@qq.com>
2016-05-26 15:26:13 +0800

    Initial Commit

---
+++ yyy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  yyy
+//
+//  Created by yanglei on 16/5/26.
+//  Copyright (c) 2016年 yanglei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ yyy/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  yyy
+//
+//  Created by yanglei on 16/5/26.
+//  Copyright (c) 2016年 yanglei. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
